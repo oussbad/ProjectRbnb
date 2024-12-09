632a3300df99bb4dfd89e0ee5ae62b41d2cf4126
oussbad <oussamabadreddine210@gmail.com>
2024-12-09 17:56:35 +0100

    first

---
+++ src/main/java/com/rbnb/rbnb/RbnbApplication.java
@@ -0,0 +1,13 @@
+package com.rbnb.rbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbnbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbnbApplication.class, args);
+    }
+
+}
